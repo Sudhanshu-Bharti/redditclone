63cb6e511b4c04808feae325bcb7a68f1cb716b1
Sudhanshu Bharti <shubham01legend@gmail.com>
2023-10-21 14:54:25 +0530

    few changes made

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import { cn } from "@/lib/utils";
 import Navbar from "../components/Navbar";
 import { Toaster } from "@/components/ui/toaster";
+import Providers from "@/components/Providers";
 export const metadata = {
   title: "Breadit",
   description: "A Reddit clone built with Next.js and TypeScript.",
@@ -26,15 +27,15 @@ export default function RootLayout({
       )}
     >
       <body className="min-h-screen pt-12 bg-slate-50 antialiased">
-        {/* @ts-expect-error server component */}
-        <Navbar />
-
-        {authModal}
-
-        <div className="container max-w-7xl mx-auto h-full pt-12">
-          {children}
-        </div>
-        <Toaster />
+        <Providers>
+          {/* @ts-expect-error server component */}
+          <Navbar />
+          {authModal}
+          <div className="container max-w-7xl mx-auto h-full pt-12">
+            {children}
+          </div>
+          <Toaster />{" "}
+        </Providers>
       </body>
     </html>
   );
