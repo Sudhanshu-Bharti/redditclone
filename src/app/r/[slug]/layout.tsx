9d488880f3904a53e1827c51a565e62a76a6a00a
Sudhanshu Bharti <120251980+Sudhanshu-Bharti@users.noreply.github.com>
2023-10-23 15:54:30 +0530

    Editor feature to add title  , code , etc
    Warning: IMAGE UPLOADING NOT WORKING

--- src/app/r/[slug]/layout.tsx
+++ src/app/r/[slug]/layout.tsx
@@ -1,7 +1,9 @@
 import SubscribeLeaveToggle from "@/components/SubscribeLeaveToggle";
+import { buttonVariants } from "@/components/ui/Button";
 import { getAuthSession } from "@/lib/auth";
 import { db } from "@/lib/db";
 import { format } from "date-fns";
+import Link from "next/link";
 import { notFound } from "next/navigation";
 import { ReactNode } from "react";
 
@@ -97,6 +99,16 @@ const Layout = async ({
                   isSubscribed={isSubsribed}
                 />
               ) : null}
+
+              <Link
+                href={`r/${slug}/submit`}
+                className={buttonVariants({
+                  variant: "outline",
+                  className: "w-full mb-6",
+                })}
+              >
+                Create Post
+              </Link>
             </dl>
           </div>
         </div>
