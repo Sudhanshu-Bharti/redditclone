7a251048bff2790477e46869c874653d7756f46b
Sudhanshu Bharti <120251980+Sudhanshu-Bharti@users.noreply.github.com>
2023-10-25 04:43:33 +0530

    view Post✅ Image ❌
    except image you can post whatever you want

---
+++ src/components/EditorOutput.tsx
@@ -0,0 +1,41 @@
+import dynamic from "next/dynamic"
+import { FC } from "react"
+
+const Output = dynamic(
+  async () => (await import("editorjs-react-renderer")).default,
+  { ssr: false },
+)
+
+interface EditorOutputProps {
+  content: any
+}
+const style = {
+  paragraph: {
+    fontSize: "0.875rem",
+    lineHeight: "1.25rem",
+  },
+}
+const renderers = {
+  code: CustomCodeRenderer,
+}
+
+const EditorOutput: FC<EditorOutputProps> = ({ content }) => {
+  return (
+    <Output
+      style={style}
+      data={content}
+      className="text-sm "
+      renderer={renderers}
+    />
+  )
+}
+
+function CustomCodeRenderer({ data }: any) {
+  return (
+    <pre className="bg-gray-800 rounded-md p-4">
+      <code className="text-gray-100 text-sm">{data.code}</code>
+    </pre>
+  )
+}
+
+export default EditorOutput
--- src/components/PostFeed.tsx
+++ src/components/PostFeed.tsx
@@ -69,11 +69,22 @@ const PostFeed: FC<PostFeedProps> = ({ initialPosts, subredditName }) => {
         if (index === posts.length - 1) {
           return (
             <li key={post.id} ref={ref}>
-              <Post />
+              <Post
+                commentAmt={post.comments.length}
+                post={post}
+                subredditName={post.subreddit.name}
+              />
             </li>
           )
         } else {
-          return <Post key={post.id} />
+          return (
+            <Post
+              commentAmt={post.comments.length}
+              post={post}
+              subredditName={post.subreddit.name}
+              key={post.id}
+            />
+          )
         }
       })}
     </ul>
